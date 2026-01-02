f10963cbee95e697dc4f98c4da7029620ecc683c
Grzegorz Lizner <grzegorz.lizner@gmail.com>
2026-01-02 11:37:58 +0100

    Terraform files added

---
+++ terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "local" {}
+
+# Create local file
+resource "local_file" "example_file" {
+  content  = "THIS IS CONTENT OF THE LOCAL FILE"
+  filename = "./my-homework-results.txt"
+}
